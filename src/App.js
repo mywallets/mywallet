841647c1f26edb3c4285dc64e6a740499b5c5621
Johnsavadkuhi <physitime@gmail.com>
2020-11-27 22:34:53 +0330

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import "./App.css";
+import myStyle from "./styles";
+import logo from "./imgs/eth.png";
 
 function App() {
+  const classes = myStyle();
+
   return (
-    <div>
-     
+    <div className={classes.body}>
+      <br />
+
+      <div className={classes.div1}>
+        Hello everyone div1
+      </div>
+      <div className={classes.div2}>hello everyone div2 </div>
+    
     </div>
   );
 }
