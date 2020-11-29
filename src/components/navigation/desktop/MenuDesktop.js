8a8228ec0b6bf42d3adf48fb4c3e530e70ae764a
Johnsavadkuhi <physitime@gmail.com>
2020-11-29 04:50:04 +0330

    create MenuDesktop component

---
+++ src/components/navigation/desktop/MenuDesktop.js
@@ -0,0 +1,24 @@
+import React from "react";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+
+export default function MenuDesktop(props){
+    const menuId = "primary-search-account-menu";
+    return <>  
+    
+    {/* <Menu
+      anchorEl={props.anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={props.open}
+      onClose={props.onClose}
+    >
+      <MenuItem onClick={props.onClick}>Wallets</MenuItem>
+      <MenuItem onClick={props.onClick}>Create</MenuItem>
+    </Menu>
+     */}
+    
+    </>
+}
