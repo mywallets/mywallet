caee205a06aa5522420b201f3a21bbabc5fe457c
Johnsavadkuhi <physitime@gmail.com>
2020-11-29 04:49:00 +0330

    crerate Home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from '../imgs/new.png'; 
+
+function Home() {
+  return (<>
+    <div className="container">
+      
+    </div>
+ </> );
+}
+
+export default Home; 
