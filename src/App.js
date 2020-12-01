93591f3d1bd0163cc4f940c32fa16e1ed30a5d2b
Johnsavadkuhi <physitime@gmail.com>
2020-12-02 03:27:49 +0330

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
-import Flags from "./components/common/Flags";
-import Wallet_Navigation from "./components/navigation/WNavgation";
-import Wallet_Router from './components/routing/'; 
+// import Flags from "./components/common/Flags";
+import WalletNavigation from "./components/navigation/WNavgation";
+import WalletRouter from './components/routing/'; 
 
 
 function App() {
 
   return <>
     
-    <Wallet_Navigation/>
-    <Flags/>
-    <Wallet_Router />
+    <WalletNavigation/>
+    {/* <Flags/> */}
+    <WalletRouter />
   
   </>
 
