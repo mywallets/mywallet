f70699680e88fa3115e4e367709c9cf0327e1d3c
Johnsavadkuhi <physitime@gmail.com>
2020-12-02 02:09:18 +0330

    remove unused codes

--- src/components/navigation/mobile/menu/WMeuItem.js
+++ src/components/navigation/mobile/menu/WMeuItem.js
@@ -2,10 +2,6 @@ import React from "react";
 import IconButton from "@material-ui/core/IconButton";
 import Badge from "@material-ui/core/Badge";
 import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
-import WalletIcon from "@material-ui/icons/AccountBalanceWallet";
-import NotificationsIcon from "@material-ui/icons/Notifications";
-import AddIcon from "@material-ui/icons/Add";
 
 export default function WMenuItem(props) {
   return (
